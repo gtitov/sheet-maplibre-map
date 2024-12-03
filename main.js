7e23301dfc6ff1ec0dc7f6a1bf1223c3d9c1446e
gman <gman>
2024-12-03 18:47:21 +0300

    list rendered features

--- main.js
+++ main.js
@@ -41,7 +41,10 @@ map.on("load", function () {
       geojson.features.map((f) => {
         document.getElementById(
           "list-all"
-        ).innerHTML += `<h4>${f.properties["Вакансия"]}</h4><a href='#' onclick="map.flyTo({center: [${f.geometry.coordinates}], zoom: 10})">Найти на карте</a><hr>`;
+        ).innerHTML += `<div class="list-item">
+        <h4>${f.properties["Вакансия"]}</h4>
+        <a href='#' onclick="map.flyTo({center: [${f.geometry.coordinates}], zoom: 10})">Найти на карте</a>
+        </div><hr>`;
       });
 
       map.addSource("vacancies", {
@@ -74,43 +77,42 @@ map.on("load", function () {
         },
       });
 
-      map.on("click", function (event) {
-        const features = map.queryRenderedFeatures(event.point, {
-          layers: ["clusters"],
+      map.on('moveend', () => {
+        const features = map.queryRenderedFeatures({
+          layers: ["clusters"]
         });
-        if (features[0].properties.cluster_id) {
-          const clusterId = features[0].properties.cluster_id;
-          const pointCount = features[0].properties.point_count;
-          const clusterSource = map.getSource("vacancies");
 
-          clusterSource
-            .getClusterLeaves(clusterId, pointCount, 0)
-            .then((clusterFeatures) => {
-              document.getElementById("list-selected").innerHTML =
-                "<h2>Выбранные вакансии</h2>";
-              clusterFeatures.map((feature) => {
-                document.getElementById(
-                  "list-selected"
-                ).innerHTML += `<h4>${feature.properties["Вакансия"]}</h4><a target="blank_" href='${feature.properties["Ссылка на сайте Картетики"]}'>Подробнее</a><hr>`;
+        document.getElementById("list-selected").innerHTML = "<h2>Сейчас на карте</h2>"
+
+
+        features.map(f => {
+          if (f.properties.cluster) {
+            const clusterId = f.properties.cluster_id;
+            const pointCount = f.properties.point_count;
+            map.getSource("vacancies").getClusterLeaves(clusterId, pointCount, 0)
+              .then((clusterFeatures) => {
+                clusterFeatures.map((feature) => document.getElementById("list-selected")
+                  .innerHTML += `<div class="list-item">
+                  <h4>${feature.properties["Вакансия"]}</h4>
+                  <a target="blank_" href='${feature.properties["Ссылка на сайте Картетики"]}'>Подробнее</a>
+                  </div><hr>`)
               });
-            });
-        } else {
-          const unclusteredFeature = features[0];
-          document.getElementById(
-            "list-selected"
-          ).innerHTML = `<h2>Выбранные вакансии</h2><h4>${unclusteredFeature.properties["Вакансия"]}</h4><a target="blank_" href='${unclusteredFeature.properties["Ссылка на сайте Картетики"]}'>Подробнее</a><hr>`;
-        }
+          } else {
+            document.getElementById("list-selected")
+              .innerHTML += `<div class="list-item">
+              <h4>${f.properties["Вакансия"]}</h4>
+              <a target="blank_" href='${f.properties["Ссылка на сайте Картетики"]}'>Подробнее</a>
+              </div><hr>`
+          }
+        })
+      })
+    })
 
-        //   console.log(features);
-      });
+  map.on("mouseenter", "clusters", function () {
+    map.getCanvas().style.cursor = "pointer";
+  });
 
-      map.on("mouseenter", "clusters", function () {
-        map.getCanvas().style.cursor = "pointer";
-      });
-
-      map.on("mouseleave", "clusters", function () {
-        map.getCanvas().style.cursor = "";
-      });
-    }
-    );
+  map.on("mouseleave", "clusters", function () {
+    map.getCanvas().style.cursor = "";
+  });
 });
